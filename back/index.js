6dddf48926c75386983526a9673de7ea2b8b43ba
chojiu <yacine@wildcodeschool.fr>
2019-12-15 22:05:26 +0100

    back setup

---
+++ back/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended : false
+}))
+app.use(cors())
+app.use(bodyParser.json())
+
+const Users = require('./routes/user')
+app.use('/users', Users)
+
+app.listen(3000, console.log('Server running'))
